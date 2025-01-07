991387f50a26a24809bc9dea14014d855dc1895e
ridesun <ridesungg@gmail.com>
2025-01-07 14:17:24 +0800

    finish 1

--- src/operators.rs
+++ src/operators.rs
@@ -77,13 +77,17 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = silu(x) * y
 // hint: this is an element-wise operation
 pub fn swiglu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+     let len = y.size();
+     assert!(len == x.size());
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
-
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+     let y = unsafe { y.data_mut() };
+     let x = x.data();
+    
+    for i in 0..len{
+        let sigmoid_x=1.0/(1.0+(-x[i]).exp());
+        let silu_x=sigmoid_x*x[i];
+        y[i]*=silu_x;
+    }
 }
 
 // C = beta * C + alpha * A @ B^T
